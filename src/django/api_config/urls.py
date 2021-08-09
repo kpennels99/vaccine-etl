552115f31cf676411fa136aeddea89113e599af0
kpennels <keaton@dotmodus.com>
2021-08-09 20:43:06 +0200

    refactored indentation

--- src/django/api_config/urls.py
+++ src/django/api_config/urls.py
@@ -43,7 +43,7 @@ urlpatterns = [
          SpectacularSwaggerView.as_view(url_name='schema'),
          name='swagger-ui'),
     path('api/schema/redoc/',
-         SpectacularRedocView.as_view(url_name='schema'),                                  
+         SpectacularRedocView.as_view(url_name='schema'),
          name='redoc'),
 ]
 
